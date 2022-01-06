093c34f9befd87a22461c2cec8948e80e5abd32b
chrisais9 <chrisais9@naver.com>
2022-01-06 14:31:57 +0900

    :sparkles: 공지 더미 데이터 추가 notice GET 라우터에 연결

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import router from "./router";
+
 const app = express();
 
 
@@ -18,5 +20,10 @@ app.get('/', (req: express.Request, res: express.Response) => {
 app.listen(3000, () => {
     console.log('Started server with 3000');
 });
+
+app.use(express.urlencoded({ limit: "100mb", extended: true })); // urlencode 지원
+app.use(express.json({ limit: "100mb" })); // json 지원
+
+app.use(router); // 라우터 연결
 
 export default app;
