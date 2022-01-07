1fb83a159cc5c1ad6a6ef800859a28fbd657ffaa
chrisais9 <chrisais9@naver.com>
2022-01-07 16:50:38 +0900

    :construction: 소켓서버 연동 테스트 완료 구현 작업중

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,6 @@ import router from "./router";
 
 const app = express();
 
-
-var requestTime = function (req, res, next) {
-    req.requestTime = Date.now();
-    next();
-};
-
-app.use(requestTime)
-
 app.get('/', (req: express.Request, res: express.Response) => {
     var responseText = 'Hello World!';
     res.send(responseText);
@@ -25,14 +17,30 @@ app.use(express.json({ limit: "100mb" })); // json 지원
 
 app.use(router); // 라우터 연결
 
-const httpServer = createServer(app)
+const httpServer = createServer(app);
+
+// http 서버를 소켓 서버로 wrapping
 const io = new Server(httpServer, {
     allowEIO3: true
-})
+});
 
-io.on("connection", (socket) => {
+interface MyFriend {
+    name: String;
+    email: String;
+}
+var friends = []
+
+io.sockets.on("connection", (socket) => {
     console.log("connected")
-})
+
+    socket.on("friend_enter", (data) => {
+        console.log("friend entered!!")
+        console.log(data)
+        friends.push({ name: data["name"], email: data["email"] })
+        socket.broadcast.emit("friend_list", friends)
+    })
+});
+
 
 httpServer.listen(EVM.PORT, () => {
     console.log('Started server with 3000');
