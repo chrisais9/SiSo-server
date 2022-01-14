1b4aeb9b3e752fb51ff941f7d30218a97097f70a
chrisais9 <chrisais9@naver.com>
2022-01-14 13:33:25 +0900

    :sparkles: 로그인 관련 로직 구현 완료

---
+++ src/modules/JWTTokenManger.ts
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
+
+import { IUserJWTToken, IUserSchema } from "../schema/User";
+import EVM from './EnvironmentVariableManager';
+import { createHttpError, SentenceKey } from './HttpError';
+
+class JWTTokenManager {
+
+    createUserJWTToken(user: IUserSchema): string {
+        let userTokenData: IUserJWTToken = {
+            user: user._id,
+            type: user.type,
+            createdAt: user.updatedAt,
+        };
+
+        return jwt.sign(userTokenData, EVM.SECRET, {
+            issuer: "api.playground.party",
+        });
+    }
+
+    verifyUserToken(jwtToken: string): IUserJWTToken {
+        try {
+            let verify = jwt.verify(jwtToken, EVM.SECRET);
+            if (!verify) throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.EXPIRED_TOKEN);
+
+            return jwt.decode(jwtToken) as IUserJWTToken;
+        } catch (err) {
+            throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.EXPIRED_TOKEN);
+        }
+    }
+}
+export default new JWTTokenManager();
--- src/schema/User.ts
+++ src/schema/User.ts
@@ -7,7 +7,11 @@
  */
 
 import { ObjectId } from "bson";
-import { Schema } from "mongoose";
+import { StatusCodes } from "http-status-codes";
+import { Document, model, Model, Schema } from "mongoose";
+import { createHttpError, SentenceKey } from "../modules/HttpError";
+import JWTTokenManger from "../modules/JWTTokenManger";
+import SocialLoginHelper, { SocialLoginInfo } from "../modules/socialLogin/SocialLoginHelper";
 import { SocialLoginStrategies } from "../modules/socialLogin/SocialLoginStrategie";
 
 
@@ -31,13 +35,82 @@ export interface IUser {
 
 export const UserSchema: Schema = new Schema(
     {
-        email: { type: String, unique: true },
         type: { type: String, enum: Object.values(SocialLoginStrategies), required: true, index: true },
-        profileImage: { type: String, default: "" },
+
+        email: { type: String, unique: true },
         userId: { type: String, required: true, unique: true, index: true },
-        userName: { type: String, required: true, unique: true, index: true }
+        userName: { type: String, required: true, unique: true, index: true },
+        profileImage: { type: String, default: "" }
     },
     {
         timestamps: true
     }
 )
+
+export interface IUserSchema extends IUser, Document {
+
+}
+
+export interface IUserModel extends Model<IUserSchema> {
+
+    loginBySocialToken(this: IUserModel, type: SocialLoginStrategies, token: string): Promise<string>
+
+    registerBySocialToken(this: IUserModel, type: SocialLoginStrategies, token: string, info?: SocialLoginInfo): Promise<string>
+
+    loginByJWTToken(this: IUserModel, jwtToken: string): Promise<IUserSchema>
+}
+
+UserSchema.statics.registerBySocialToken = async function (this: IUserModel, type: SocialLoginStrategies, token: string): Promise<string> {
+    try {
+        let socialInfo = await SocialLoginHelper.socialLoginInformation(type, token)
+        if (!socialInfo) throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.BAD_TOKEN, "잘못된 소셜 토큰")
+
+        let user: IUserSchema = await this.findOne({ userId: socialInfo.userId }, { type: 1, userId: 1, updatedAt: 1 })
+        if (user) {
+            user.updatedAt = new Date()
+            await user.save()
+
+            let jwtToken = JWTTokenManger.createUserJWTToken(user)
+
+            return jwtToken
+        } else {
+            return await this.registerBySocialToken(type, token)
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+UserSchema.statics.registerBySocialToken = async function (this: IUserModel, type: SocialLoginStrategies, token: string, info?: SocialLoginInfo): Promise<string> {
+    if (!info) info = await SocialLoginHelper.socialLoginInformation(type, token)
+    if (!info) throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.BAD_TOKEN, "잘못된 소셜 토큰")
+
+    let user: IUserSchema = await this.findOne({ userId: info.userId }, { userId: 1 })
+    if (user) throw createHttpError(StatusCodes.CONFLICT, SentenceKey.ALREADY_REGISTERED, "이미 가입된 계정");
+
+    let newUser = new this({
+        type: info.type,
+        userId: info.userId,
+        userName: info.userName,
+        profileImage: info.profileImage || ""
+    })
+
+    try {
+        return JWTTokenManger.createUserJWTToken(await newUser.save())
+    } catch (error) {
+        throw createHttpError(StatusCodes.CONFLICT, SentenceKey.ALREADY_REGISTERED, "이미 가입된 계정")
+    }
+}
+
+UserSchema.statics.loginByJWTToken = async function (this: IUserModel, jwtToken: SocialLoginStrategies): Promise<IUserSchema> {
+    let tokenData = JWTTokenManger.verifyUserToken(jwtToken)
+    if (!tokenData) throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.EXPIRED_TOKEN, "만료된 JWT 토큰");
+
+    let user = await this.findOne({ _id: tokenData.user });
+    if (!user) throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.BAD_TOKEN, "잘못된 JWT 토큰");
+
+    user.updatedAt = new Date();
+
+    return await user.save();
+}
+export default model<IUserSchema>("User", UserSchema) as IUserModel
