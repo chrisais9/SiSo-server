3fdc953c447f5431ce75c779bd56171820c07edd
chrisais9 <chrisais9@naver.com>
2022-01-18 16:25:04 +0900

    :sparkles: 페이스북 프로필 정보 가져오는 기능 추가

---
+++ src/modules/socialLogin/strategy/Facebook.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { StatusCodes } from "http-status-codes";
+import { createHttpError, SentenceKey } from "../../HttpError";
+import { SocialLoginInfo } from "../SocialLoginHelper";
+import SocialLoginStrategy, { SocialLoginStrategies } from "../SocialLoginStrategies";
+
+class Facebook extends SocialLoginStrategy {
+    async tokenInfo(token: string): Promise<SocialLoginInfo> {
+        // https://developers.facebook.com/docs/graph-api/using-graph-api/
+        try {
+            let userData = (
+                await axios.get("https://graph.facebook.com/v2.10/me", {
+                    params: {
+                        fields: "email,name",
+                        access_token: token,
+                    },
+                })
+            ).data;
+
+            return {
+                type: SocialLoginStrategies.FACEBOOK,
+                email: userData.email,
+                userId: userData.id,
+                userName: userData.name,
+                profileImage: `https://graph.facebook.com/v2.10/${userData.id}/picture?type=normal`,
+            };
+        } catch (err) {
+            throw createHttpError(StatusCodes.UNAUTHORIZED, SentenceKey.BAD_TOKEN, "잘못된 소셜 토큰");
+        }
+    }
+}
+export default new Facebook();
